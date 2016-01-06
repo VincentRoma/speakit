55aeea34850d2604674d9667ef20cad48d9bc9e8
VincentRoma <roma.vincent@gmail.com>
2016-01-06 15:07:42 +0100

    Doesn't duplication discussion anymore

--- src/ChatBundle/Controller/DiscussionController.php
+++ src/ChatBundle/Controller/DiscussionController.php
@@ -15,23 +15,36 @@ class DiscussionController extends Controller
 
     public function createAction($id)
     {
+        $user = $this->getUser();
         // Get requested user
         $em = $this->getDoctrine()->getManager();
         $invited = $em->getRepository('EduSpeakBundle:User')->findOneById($id);
         if($invited){
-            // Create new discussion
-            $discussion = new Discussion();
-            $user = $this->getUser();
+            $discussion = false;
+            // Check for existing discussion
+            foreach($user->getDiscussions() as $d){
+                foreach($d->getParticipants() as $p) {
+                    if ($invited->getId() == $p->getId()) {
+                        $discussion = $d;
+                    }
+                }
+            }
+            if($discussion){
+                return $this->redirectToRoute('chat_display', array('id'=>$discussion->getId()));
+            }else {
+                // Create new discussion
+                $discussion = new Discussion();
 
-            //Add Participants
-            $discussion = $discussion->addParticipant($user);
-            $discussion = $discussion->addParticipant($invited);
-            $invited->addDiscussion($discussion);
-            $user->addDiscussion($discussion);
-            $em->persist($discussion);
-            $em->flush();
+                //Add Participants
+                $discussion = $discussion->addParticipant($user);
+                $discussion = $discussion->addParticipant($invited);
+                $invited->addDiscussion($discussion);
+                $user->addDiscussion($discussion);
+                $em->persist($discussion);
+                $em->flush();
 
-            return $this->render('ChatBundle:Discussion:discussion.html.twig', array('discussion' => $discussion));
+                return $this->redirectToRoute('chat_display', array('id'=>$discussion->getId()));
+            }
         }else{
             return $this->render('ChatBundle:Discussion:discussion_fail.html.twig');
         }
@@ -44,5 +57,16 @@ class DiscussionController extends Controller
         $user = $this->getUser();
         $discussions = $user->getDiscussions()->toArray();
         return $this->render('ChatBundle:Discussion:discussions.html.twig', array('discussions' => $discussions));
+    }
+
+    public function displayAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $discussion = $em->getRepository('ChatBundle:Discussion')->findOneById($id);
+        if($discussion){
+            return $this->render('ChatBundle:Discussion:discussion.html.twig', array('discussion' => $discussion));
+        }else{
+            return $this->render('ChatBundle:Discussion:discussion_fail.html.twig');
+        }
     }
 }
